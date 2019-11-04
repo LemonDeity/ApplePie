44a3f0d9148d89e73ef7c8787f724fae223f21fd
Noah R <nrange2001@gmail.com>
2019-11-04 11:27:14 -0600

    j

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,10 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Range, Noah H on 11/4/19.
+//  Copyright © 2019 Range, Noah H. All rights reserved.
+//
+
+import Foundation
+
--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -13,9 +13,17 @@ class ViewController: UIViewController {
     @IBOutlet var correctWordLabel: UILabel!
     @IBOutlet var scoreLabel: UILabel!
     @IBOutlet var letterButtons: [UIButton]!
+    let listOfWords:[String] = ["pizza","green","soccer","basketball"]
+    var totalWin = 0
+    var totalLoss = 0
+    let incorrectMoves = 7
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func newRound(){
+        
     }
 
     @IBAction func buttonTapped(_ sender: UIButton) {
